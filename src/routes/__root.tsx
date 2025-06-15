d8284b395f14a477adb894b6cfe549024050e836
Aiden <brain1401@gmail.com>
2025-06-15 13:18:02 +0900

    feat: 루트 레이아웃에 검색 바 헤더 추가

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,6 +7,7 @@ import type { QueryClient } from "@tanstack/react-query";
 import TopNavBar from "@/components/layout/TopNavBar.tsx";
 import QuickLinksBar from "@/components/layout/QuickLinksBar.tsx";
 import Footer from "@/components/layout/Footer.tsx";
+import SearchBarHeader from "@/components/route/index/search/SearchBarHeader.tsx";
 
 interface MyRouterContext {
   queryClient: QueryClient;
@@ -17,6 +18,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     <div className="-mr-scrollbar flex min-h-[100dvh] flex-col bg-gray-50 font-nanum_square_neo_variable font-[500]">
       <TopNavBar />
       <QuickLinksBar />
+      <SearchBarHeader />
       <main className="container mx-auto flex max-w-7xl flex-1 flex-col px-8 py-5">
         <Outlet />
       </main>
