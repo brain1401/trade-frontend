015539288d5a85fb329ca103024b3a1e79f0e320
Aiden <brain1401@gmail.com>
2025-06-28 00:59:35 +0900

    refactor: API 모듈 통합 내보내기 개선 - 각 API 모듈을 직접 내보내어 타입스크립트 모듈 해석 용이성 및 개발 편의성 향상

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,22 +1,13 @@
 /**
- * 통합 API 모듈
- * 모든 도메인별 API를 중앙에서 관리
+ * @file API 모듈 통합 내보내기 (Unified API Module Exports)
+ * @description
+ * 각 API 모듈을 직접 내보내어 타입스크립트의 모듈 해석을 용이하게 하고
+ * 개발 편의성을 높힌다. 코드 스플리팅이 필요할 경우,
+ * 각 기능을 사용하는 컴포넌트 레벨에서 React.lazy 등을 활용하는 것을 권장
  */
-
-// 공통 모듈
-export * from "./common";
-
-// 도메인별 API 모듈
+export * from "./bookmark";
 export * from "./chat";
+export * from "./common";
 export * from "./exchange-rates";
 export * from "./news";
 export * from "./notification";
-export * from "./bookmark";
-
-// 하위 호환성을 위한 기본 export
-export { httpClient, ApiError } from "./common";
-export { chatApi, chatHistoryApi } from "./chat";
-export { exchangeRatesApi } from "./exchange-rates";
-export { newsApi } from "./news";
-export { notificationApi } from "./notification";
-export { bookmarkApi } from "./bookmark";
