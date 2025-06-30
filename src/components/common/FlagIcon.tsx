37a44af92aae50323b16cafb7392b00691a79aab
Aiden <brain1401@gmail.com>
2025-07-01 03:59:35 +0900

    refactor: 국기 아이콘 컴포넌트에 클래스 이름 속성 추가 - 이미지 클래스 이름을 통해 스타일링 가능하도록 수정

--- src/components/common/FlagIcon.tsx
+++ src/components/common/FlagIcon.tsx
@@ -1,14 +1,23 @@
 import type { ImgHTMLAttributes } from "react";
+import { cn } from "@/lib/utils/cn";
+
+type Props = ImgHTMLAttributes<HTMLImageElement> & {
+  className?: string;
+};
 
 /**
  * 국기 아이콘 컴포넌트
  * @param src 국기 이미지 URL
  * @param alt 이미지 대체 텍스트
+ * @param className 이미지 클래스 이름
  */
-export function FlagIcon({
-  src,
-  alt,
-  ...props
-}: ImgHTMLAttributes<HTMLImageElement>) {
-  return <img className="h-[2.5rem]" src={src} alt={alt} {...props} />;
+export function FlagIcon({ className, src, alt, ...props }: Props) {
+  return (
+    <img
+      className={cn("h-[2.5rem]", className)}
+      src={src}
+      alt={alt}
+      {...props}
+    />
+  );
 }
