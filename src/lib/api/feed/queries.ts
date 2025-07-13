70ba2e50b7d1dc9c4f8ab836063526b684ae88cc
Aiden <brain1401@gmail.com>
2025-07-13 21:43:42 +0900

    refactor: useRecentUpdatesFeed 및 useMarkFeedAsRead 커스텀 Hook으로 변경 및 코드 정리

--- src/lib/api/feed/queries.ts
+++ src/lib/api/feed/queries.ts
@@ -5,43 +5,41 @@ import {
   useMutation,
   useQueryClient,
 } from "@tanstack/react-query";
-import type { RecentUpdatesFeedData } from "./types";
 
 export const feedQueryKeys = {
   all: () => ["feed"] as const,
   recentUpdates: () => [...feedQueryKeys.all(), "recent-updates"] as const,
 };
 
-export const feedQueries = {
-  recentUpdates: () =>
-    useInfiniteQuery({
-      queryKey: feedQueryKeys.recentUpdates(),
+// 커스텀 Hook으로 변경
+export const useRecentUpdatesFeed = () =>
+  useInfiniteQuery({
+    queryKey: feedQueryKeys.recentUpdates(),
 
-      queryFn: ({ pageParam }) => {
-        const pageNumber = typeof pageParam === "number" ? pageParam : 0;
-        return feedApi.getRecentUpdatesFeed({ pageParam: pageNumber });
-      },
+    queryFn: ({ pageParam }) => {
+      const pageNumber = typeof pageParam === "number" ? pageParam : 0;
+      return feedApi.getRecentUpdatesFeed({ pageParam: pageNumber });
+    },
 
-      initialPageParam: 0,
+    initialPageParam: 0,
 
-      getNextPageParam: (lastPage) => {
-        if (lastPage.last) {
-          return undefined;
-        }
-        return lastPage.number + 1;
-      },
-      staleTime: 1000 * 60 * 5, // 5분
-    }),
+    getNextPageParam: (lastPage) => {
+      if (lastPage.last) {
+        return undefined;
+      }
+      return lastPage.number + 1;
+    },
+    staleTime: 1000 * 60 * 5, // 5분
+  });
 
-  useMarkFeedAsRead: () => {
-    const queryClient = useQueryClient();
-    return useMutation<void, ApiError, number>({
-      mutationFn: (feedId: number) => feedApi.markFeedAsRead(feedId),
-      onSuccess: () => {
-        return queryClient.invalidateQueries({
-          queryKey: feedQueryKeys.recentUpdates(),
-        });
-      },
-    });
-  },
+export const useMarkFeedAsRead = () => {
+  const queryClient = useQueryClient();
+  return useMutation<void, ApiError, number>({
+    mutationFn: (feedId: number) => feedApi.markFeedAsRead(feedId),
+    onSuccess: () => {
+      return queryClient.invalidateQueries({
+        queryKey: feedQueryKeys.recentUpdates(),
+      });
+    },
+  });
 };
