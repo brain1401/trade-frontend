8bc32e9c8f406f39b45011b8d39b0e5561911731
Aiden <brain1401@gmail.com>
2025-07-13 20:35:56 +0900

    style: 메인 콘텐츠 영역에 패딩 추가하여 레이아웃 개선

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -55,7 +55,7 @@ function RootLayout() {
       }`}
     >
       {isMobile ? <TopBar /> : <SideBar />}
-      <main className="flex-1 overflow-y-auto">
+      <main className="flex-1 overflow-y-auto px-[2.5rem]">
         <Outlet />
       </main>
       <TanStackQueryLayout />
